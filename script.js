c88619d81b01273458c0502b11c931932a6c10f1
ShinJaehun <gunsmoke.shin@gmail.com>
2022-10-03 16:21:20 +0900

    collision detection

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ canvas.height = 600;
 const cellSize = 100;
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let defenderCost = 100;
+
+const mouse = {
+    x: 10,
+    y: 10,
+    width: 0.1,
+    height: 0.1,
+}
+
+let canvasPosition = canvas.getBoundingClientRect()
+// console.log(canvasPosition);
+canvas.addEventListener('mousemove', function(e){
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+});
+canvas.addEventListener('mouseleave', function(){
+    mouse.x = undefined;
+    mouse.y = undefined;
+});
 
 // game board
 const controlsBar = {
@@ -14,15 +34,62 @@ const controlsBar = {
     height: cellSize, 
 }
 
+class Cell {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+    draw(){
+        if (mouse.x && mouse.y && collision(this, mouse)) {
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
+    }
+}
+
+function createGrid(){
+    for (let y = cellSize; y < canvas.height; y += cellSize){
+        for (let x = 0; x < canvas.width; x += cellSize) {
+            gameGrid.push(new Cell(x, y));
+        }
+    }
+}
+
+createGrid();
+
+function handleGameGrid(){
+    for (let i = 0; i < gameGrid.length; i++) {
+        gameGrid[i].draw()
+    }
+}
+
+// console.log(gameGrid);
+
 // projectiles
 // defenders
+
+
 // enemies
 // resources
 // utilities
 
 function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
+    handleGameGrid();
     requestAnimationFrame(animate); // recursion
 }
 animate();
+
+function collision(first, second){
+    if ( !( first.x > second.x + second.width ||
+            first.x + first.width < second.x ||
+            first.y > second.y + second.height ||
+            first.y + first.height < second.y)
+    ) {
+        return true;
+    };
+};
